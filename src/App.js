8cec0c5810b9f0e304a338fa1cef7fc773d898a4
Michael Baldanza <michael.snider.baldanza@gmail.com>
2023-01-04 10:43:05 -0500

    implement header element with flexbox, and update font-family, background-color and color

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div id="flex-container">
+          <a className="unlink" id="page-title" href="#">swinkerly.</a>
+          <div id="login-btn">
+            <a className="unlink" href="#">login</a>
+          </div>
+        </div>
       </header>
     </div>
   );
